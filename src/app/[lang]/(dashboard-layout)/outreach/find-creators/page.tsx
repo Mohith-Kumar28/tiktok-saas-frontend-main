dbb0df31f1da357d48059bf19b5877ee9e239428
mohith-kumar28 <mohithkumar808@gmail.com>
2025-05-30 10:24:56 +0530

    Update Find Creators page to accept search parameters as props

--- src/app/[lang]/(dashboard-layout)/outreach/find-creators/page.tsx
+++ src/app/[lang]/(dashboard-layout)/outreach/find-creators/page.tsx
@@ -1,9 +1,15 @@
 import FindCreatorsViewPage from "@/features/find-creators/find-creators-view-page"
 
+import type { TSearchParamsType } from "@/types/types"
+
 export const metadata = {
   title: "Dashboard : Find Creators",
 }
 
-export default async function page() {
-  return <FindCreatorsViewPage />
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: TSearchParamsType
+}) {
+  return <FindCreatorsViewPage searchParams={searchParams} />
 }
